d32f8138dd17f2f0f4ae1618b385ae1af9668d27
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2026-03-08 20:53:06 -0500

    Initial Commit

---
+++ CursoiOS/CursoiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursoiOSApp.swift
+//  CursoiOS
+//
+//  Created by Camilo on 3/8/26.
+//
+
+import SwiftUI
+
+@main
+struct CursoiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
