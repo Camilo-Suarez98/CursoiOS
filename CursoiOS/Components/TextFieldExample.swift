3d515b7043ad3f8393c71a2a245e9a3a92a6f2b3
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2026-03-09 21:22:31 -0500

    feat: create fieldtext component with text and password with styles and action

--- CursoiOS/Components/TextFieldExample.swift
+++ CursoiOS/Components/TextFieldExample.swift
@@ -8,8 +8,30 @@
 import SwiftUI
 
 struct TextFieldExample: View {
+    @State var email = ""
+    @State var password = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TextField("Escribe tu email", text: $email)
+            .keyboardType(.emailAddress)
+            .font(.system(size: 25))
+            .padding(16)
+            .background(.gray.opacity(0.2))
+            .cornerRadius(16)
+            .padding(.horizontal, 20)
+            .onChange(of: email) { oldValue, newValue in
+                print("Antiguo email era: \(oldValue) y el nuevo es \(newValue)")
+            }
+        
+        SecureField("Escribe tu contraseña", text: $password)
+            .keyboardType(.default)
+            .font(.system(size: 25))
+            .padding(16)
+            .background(.gray.opacity(0.2))
+            .cornerRadius(16)
+            .padding(.horizontal, 20)
+            .onChange(of: password) { oldValue, newValue in
+                print("Antiguo contraseña era: \(oldValue) y la nueva es \(newValue)")
+            }
     }
 }
 
