1cd87306ddfdc320a8086440513b43103af6fa12
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2026-03-09 21:21:07 -0500

    feat: create fieldtext component with text and password

---
+++ CursoiOS/Components/TextFieldExample.swift
@@ -0,0 +1,18 @@
+//
+//  TextFieldExample.swift
+//  CursoiOS
+//
+//  Created by Camilo on 3/9/26.
+//
+
+import SwiftUI
+
+struct TextFieldExample: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextFieldExample()
+}
