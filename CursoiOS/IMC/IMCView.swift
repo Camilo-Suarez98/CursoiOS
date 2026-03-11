78f95688217d1f5e9958787a1c2b3b1f04e52b79
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2026-03-10 19:42:20 -0500

    feat: add imc app with components

---
+++ CursoiOS/IMC/IMCView.swift
@@ -0,0 +1,18 @@
+//
+//  IMCView.swift
+//  CursoiOS
+//
+//  Created by Camilo on 3/10/26.
+//
+
+import SwiftUI
+
+struct IMCView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IMCView()
+}
