5c66798e66a0d1fc7b31f5481e2e0d7edab80a56
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2026-03-11 18:53:33 -0500

    feat: create counter view to change age and weight

---
+++ CursoiOS/IMC/Components/CounterView.swift
@@ -0,0 +1,18 @@
+//
+//  CounterView.swift
+//  CursoiOS
+//
+//  Created by Camilo on 3/10/26.
+//
+
+import SwiftUI
+
+struct CounterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CounterView()
+}
