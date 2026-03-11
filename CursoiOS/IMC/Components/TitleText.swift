898d7fbe11a6c9bf8cfd05704f2feb8e079d77dc
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2026-03-10 20:55:48 -0500

    feat: create a title text and height calculator component

---
+++ CursoiOS/IMC/Components/TitleText.swift
@@ -0,0 +1,18 @@
+//
+//  TitleText.swift
+//  CursoiOS
+//
+//  Created by Camilo on 3/10/26.
+//
+
+import SwiftUI
+
+struct TitleText: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TitleText()
+}
