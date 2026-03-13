4e40029ab21eb66e564d5d656b86427e87a76b77
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2026-03-12 21:22:02 -0500

    feat: create imc calculate button component and result view to handle the result in a new view

---
+++ CursoiOS/IMC/Views/Components/IMCCalculateButton.swift
@@ -0,0 +1,18 @@
+//
+//  IMCCalculateButton.swift
+//  CursoiOS
+//
+//  Created by Camilo on 3/12/26.
+//
+
+import SwiftUI
+
+struct IMCCalculateButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IMCCalculateButton()
+}
---
+++ CursoiOS/IMC/Views/Components/InformationResultView.swift
@@ -0,0 +1,18 @@
+//
+//  InformationResultView.swift
+//  CursoiOS
+//
+//  Created by Camilo on 3/12/26.
+//
+
+import SwiftUI
+
+struct InformationResultView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InformationResultView()
+}
