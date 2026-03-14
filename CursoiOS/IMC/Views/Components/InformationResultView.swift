52b9c5e2dc872132e34827e1c53e5446ecc97e5b
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2026-03-13 21:32:07 -0500

    feat: config information to show title, description and color to show depending on imc result

--- CursoiOS/IMC/Views/Components/InformationResultView.swift
+++ CursoiOS/IMC/Views/Components/InformationResultView.swift
@@ -11,22 +11,58 @@ struct InformationResultView: View {
     let result: Double
     
     var body: some View {
-        VStack {
-            Text("IMC")
-                .foregroundStyle(.green)
+        let resultInformation = getImcResult(result: result)
+        
+        VStack(spacing: 50) {
+            Text(resultInformation.0)
+                .foregroundStyle(resultInformation.2)
                 .font(.title)
                 .bold()
             Text("\(result, specifier: "%.2f")")
                 .font(.system(size: 80))
                 .bold()
-            Text("Este es un resultado normal")
+            Text(resultInformation.1)
                 .font(.title2)
-                .bold()
+                .padding(.horizontal, 8)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.backgroundComponent)
+        .cornerRadius(16)
+        .padding(16)
     }
 }
 
+func getImcResult(result: Double) -> (String, String, Color) {
+    let title: String
+    let description: String
+    let color: Color
+    
+    switch result {
+    case 0.00...19.99:
+        title = "Peso Bajo"
+        description = "Peso bajo según el IMC"
+        color = .yellow
+    case 20.00...24.99:
+        title = "Peso Normal"
+        description = "Estas en el peso recomendado según el IMC"
+        color = .green
+    case 25.00...29.99:
+        title = "Sobrepeso"
+        description = "Estas por encima del peso recomendado según el IMC"
+        color = .orange
+    case 30.00...100,00:
+        title = "Obesidad"
+        description = "Estas muy por encima del peso recomendado según el IMC"
+        color = .red
+    default:
+        title = "Error"
+        description = "Ha ocurrido un error"
+        color = .gray
+    }
+    
+    return (title, description, color)
+}
+
 #Preview {
-    InformationResultView(result: 5)
+    InformationResultView(result: 100)
 }
