28284f0907bd233cb10e9e3dc39c77ee638eb821
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2026-03-14 11:11:09 -0500

    feat: create new app about superheros finder

---
+++ CursoiOS/Superhero/Views/SuperheroFinderView.swift
@@ -0,0 +1,18 @@
+//
+//  SuperheroSearcher.swift
+//  CursoiOS
+//
+//  Created by Camilo on 3/14/26.
+//
+
+import SwiftUI
+
+struct SuperheroSearcher: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    SuperheroSearcher()
+}
