23b0e80a1485efe5843ce09a9f6524aaa1a2b93c
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2026-03-14 22:54:29 -0500

    feat: config env variable to use the superhero api key

--- CursoiOS/Config.swift
+++ CursoiOS/Config.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+enum Config {
+    static var superheroApiToken: String {
+        guard let token = Bundle.main.object(forInfoDictionaryKey: "SUPERHERO_API_TOKEN") as? String else {
+            fatalError("SUPERHERO_API_TOKEN no encontrado")
+        }
+        return token
+    }
+}
