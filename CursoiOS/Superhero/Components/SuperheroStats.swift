fd1d11e9b359e520442492448e9af60b55eb0ad1
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2026-03-15 21:40:53 -0500

    feat: create stats of superhero by id

---
+++ CursoiOS/Superhero/Components/SuperheroStats.swift
@@ -0,0 +1,18 @@
+//
+//  SuperheroStats.swift
+//  CursoiOS
+//
+//  Created by Camilo on 3/15/26.
+//
+
+import SwiftUI
+
+struct SuperheroStats: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SuperheroStats()
+}
