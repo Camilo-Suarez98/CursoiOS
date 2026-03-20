82b5810deb5634911b156a6407b45960b90a3847
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2026-03-19 20:58:49 -0500

    feat: create map view example

---
+++ CursoiOS/Components/MapExample.swift
@@ -0,0 +1,18 @@
+//
+//  MapExample.swift
+//  CursoiOS
+//
+//  Created by Camilo on 3/19/26.
+//
+
+import SwiftUI
+
+struct MapExample: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapExample()
+}
