2ac09a46f1b26eb2ab116c27a4d3b4dbf9aecc57
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2026-03-19 20:59:42 -0500

    feat: include coordinates for the map with example of 2 positions

--- CursoiOS/Components/MapExample.swift
+++ CursoiOS/Components/MapExample.swift
@@ -6,10 +6,71 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapExample: View {
+    @State var position = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(
+                latitude: 7.1091338,
+                longitude: -73.1237504
+            ), span: MKCoordinateSpan(
+                latitudeDelta: 0.01,
+                longitudeDelta: 0.01
+            )
+        )
+    )
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Map(position: $position)
+                .mapStyle(
+                    .hybrid(
+                        elevation: .flat,
+                        showsTraffic: true
+                    )
+            )
+            
+            VStack {
+                Spacer()
+                HStack {
+                    Button("Punto 1") {
+                        position = MapCameraPosition.region(
+                            MKCoordinateRegion(
+                                center: CLLocationCoordinate2D(
+                                    latitude: 7.0674039,
+                                    longitude: -73.1032497
+                                ), span: MKCoordinateSpan(
+                                    latitudeDelta: 0.01,
+                                    longitudeDelta: 0.01
+                                )
+                            )
+                        )
+                    }
+                    .padding(16)
+                    .background(.white)
+                    .padding()
+                    
+                    
+                    Button("Punto 2") {
+                        position = MapCameraPosition.region(
+                            MKCoordinateRegion(
+                                center: CLLocationCoordinate2D(
+                                    latitude: 7.0628089,
+                                    longitude: -73.0846552
+                                ), span: MKCoordinateSpan(
+                                    latitudeDelta: 0.01,
+                                    longitudeDelta: 0.01
+                                )
+                            )
+                        )
+                    }
+                    .padding(16)
+                    .background(.white)
+                    .padding()
+                }
+            }
+        }
     }
 }
 
