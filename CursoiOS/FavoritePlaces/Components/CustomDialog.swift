0a2f6c4113c506d6bb25ccc210bdd3f9d85243bd
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2026-03-22 19:33:47 -0500

    feat: create custom diaglo component

---
+++ CursoiOS/FavoritePlaces/Components/CustomDialog.swift
@@ -0,0 +1,18 @@
+//
+//  CustomDialog.swift
+//  CursoiOS
+//
+//  Created by Camilo on 3/22/26.
+//
+
+import SwiftUI
+
+struct CustomDialog: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomDialog()
+}
